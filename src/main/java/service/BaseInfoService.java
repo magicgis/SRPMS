be7ad241e43663a6b924e45a36d8aee246e8e439
Guofan Xu <imxgfan@live.com>
2015-06-16 19:17:41 +0800

    去除接口中无用的部分

--- src/main/java/service/BaseInfoService.java
+++ src/main/java/service/BaseInfoService.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public interface BaseInfoService extends BaseService<BaseInfo> {
 
-    List<BaseInfo> search(String keyword, String sort, String order);
-
     List getByTableName(String tableName);
 
     boolean addBaseInfo(BaseInfo baseInfo);
--- src/main/java/service/MagService.java
+++ src/main/java/service/MagService.java
@@ -2,8 +2,5 @@ package service;
 
 import entity.Mag;
 
-import java.util.List;
-
 public interface MagService extends BaseService<Mag> {
-    List<Mag> search(String keyword, String sort, String order);
 }
