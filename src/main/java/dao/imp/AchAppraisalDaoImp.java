dcaa6013f7d76ccf6282ba4cbf7c2b93bcba802f
Guofan Xu <imxgfan@live.com>
2015-10-12 16:51:21 +0800

    添加成果坚定

---
+++ src/main/java/dao/imp/AchAppraisalDaoImp.java
@@ -0,0 +1,12 @@
+package dao.imp;
+
+import dao.AchAppraisalDao;
+import entity.AchAppraisal;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by guofan on 2015/10/12.
+ */
+@Repository
+public class AchAppraisalDaoImp extends BaseDaoImp<AchAppraisal> implements AchAppraisalDao {
+}
