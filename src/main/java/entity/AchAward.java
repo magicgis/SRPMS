3f40d79baa56b58238ae04392937c665cb78f9eb
Guofan Xu <imxgfan@live.com>
2015-10-12 17:19:49 +0800

    获奖实体

---
+++ src/main/java/entity/AchAward.java
@@ -0,0 +1,175 @@
+package entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created by guofan on 2015/10/12.
+ */
+@Entity
+@Table(name = "ach_award", schema = "", catalog = "srpms")
+public class AchAward {
+    private String id;
+    private String name;
+    private String date;
+    private String unit;
+    private String result;
+    private String way;
+    private Integer score;
+    private String arg;
+    private String process;
+    private BaseInfo dept;
+    private Standard standard;
+
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "id", nullable = false, insertable = true, updatable = true, length = 255)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "date", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Basic
+    @Column(name = "unit", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    @Basic
+    @Column(name = "result", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    @Basic
+    @Column(name = "way", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getWay() {
+        return way;
+    }
+
+    public void setWay(String way) {
+        this.way = way;
+    }
+
+    @Basic
+    @Column(name = "score", nullable = true, insertable = true, updatable = true)
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    @Basic
+    @Column(name = "arg", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getArg() {
+        return arg;
+    }
+
+    public void setArg(String arg) {
+        this.arg = arg;
+    }
+
+    @Basic
+    @Column(name = "process", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getProcess() {
+        return process;
+    }
+
+    public void setProcess(String process) {
+        this.process = process;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AchAward achAward = (AchAward) o;
+
+        if (id != null ? !id.equals(achAward.id) : achAward.id != null) return false;
+        if (name != null ? !name.equals(achAward.name) : achAward.name != null) return false;
+        if (date != null ? !date.equals(achAward.date) : achAward.date != null) return false;
+        if (unit != null ? !unit.equals(achAward.unit) : achAward.unit != null) return false;
+        if (result != null ? !result.equals(achAward.result) : achAward.result != null) return false;
+        if (way != null ? !way.equals(achAward.way) : achAward.way != null) return false;
+        if (score != null ? !score.equals(achAward.score) : achAward.score != null) return false;
+        if (arg != null ? !arg.equals(achAward.arg) : achAward.arg != null) return false;
+        if (process != null ? !process.equals(achAward.process) : achAward.process != null) return false;
+        if (dept != null ? !dept.equals(achAward.dept) : achAward.dept != null) return false;
+        return !(standard != null ? !standard.equals(achAward.standard) : achAward.standard != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result1 = id != null ? id.hashCode() : 0;
+        result1 = 31 * result1 + (name != null ? name.hashCode() : 0);
+        result1 = 31 * result1 + (date != null ? date.hashCode() : 0);
+        result1 = 31 * result1 + (unit != null ? unit.hashCode() : 0);
+        result1 = 31 * result1 + (result != null ? result.hashCode() : 0);
+        result1 = 31 * result1 + (way != null ? way.hashCode() : 0);
+        result1 = 31 * result1 + (score != null ? score.hashCode() : 0);
+        result1 = 31 * result1 + (arg != null ? arg.hashCode() : 0);
+        result1 = 31 * result1 + (process != null ? process.hashCode() : 0);
+        result1 = 31 * result1 + (dept != null ? dept.hashCode() : 0);
+        result1 = 31 * result1 + (standard != null ? standard.hashCode() : 0);
+        return result1;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "dept", referencedColumnName = "id")
+    public BaseInfo getDept() {
+        return dept;
+    }
+
+    public void setDept(BaseInfo dept) {
+        this.dept = dept;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "standard", referencedColumnName = "id")
+    public Standard getStandard() {
+        return standard;
+    }
+
+    public void setStandard(Standard standard) {
+        this.standard = standard;
+    }
+
+}
