d2792bde91f42d703fe7327628230ba5bf7b73c8
Guofan Xu <imxgfan@live.com>
2015-11-09 14:16:37 +0800

    更新用户结构

--- src/main/java/entity/Staff.java
+++ src/main/java/entity/Staff.java
@@ -16,12 +16,9 @@ public class Staff {
     private String id;
     private String name;
     private String idCard;
-    private String edu;
     private String position;
     private String degree;
-    private String staSnm;
     private String memo;
-    private BaseInfo dept;
     private BaseInfo rank;
     private BaseInfo col;
     private User user;
@@ -57,16 +54,6 @@ public class Staff {
     }
 
     @Basic
-    @Column(name = "edu")
-    public String getEdu() {
-        return edu;
-    }
-
-    public void setEdu(String edu) {
-        this.edu = edu;
-    }
-
-    @Basic
     @Column(name = "position")
     public String getPosition() {
         return position;
@@ -84,16 +71,6 @@ public class Staff {
 
     public void setDegree(String degree) {
         this.degree = degree;
-    }
-
-    @Basic
-    @Column(name = "sta_snm")
-    public String getStaSnm() {
-        return staSnm;
-    }
-
-    public void setStaSnm(String staSnm) {
-        this.staSnm = staSnm;
     }
 
     @Basic
@@ -116,12 +93,9 @@ public class Staff {
         if (id != null ? !id.equals(staff.id) : staff.id != null) return false;
         if (name != null ? !name.equals(staff.name) : staff.name != null) return false;
         if (idCard != null ? !idCard.equals(staff.idCard) : staff.idCard != null) return false;
-        if (edu != null ? !edu.equals(staff.edu) : staff.edu != null) return false;
         if (position != null ? !position.equals(staff.position) : staff.position != null) return false;
         if (degree != null ? !degree.equals(staff.degree) : staff.degree != null) return false;
-        if (staSnm != null ? !staSnm.equals(staff.staSnm) : staff.staSnm != null) return false;
         if (memo != null ? !memo.equals(staff.memo) : staff.memo != null) return false;
-        if (dept != null ? !dept.equals(staff.dept) : staff.dept != null) return false;
         if (rank != null ? !rank.equals(staff.rank) : staff.rank != null) return false;
         if (col != null ? !col.equals(staff.col) : staff.col != null) return false;
         return !(user != null ? !user.equals(staff.user) : staff.user != null);
@@ -133,26 +107,13 @@ public class Staff {
         int result = id != null ? id.hashCode() : 0;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (idCard != null ? idCard.hashCode() : 0);
-        result = 31 * result + (edu != null ? edu.hashCode() : 0);
         result = 31 * result + (position != null ? position.hashCode() : 0);
         result = 31 * result + (degree != null ? degree.hashCode() : 0);
-        result = 31 * result + (staSnm != null ? staSnm.hashCode() : 0);
         result = 31 * result + (memo != null ? memo.hashCode() : 0);
-        result = 31 * result + (dept != null ? dept.hashCode() : 0);
         result = 31 * result + (rank != null ? rank.hashCode() : 0);
         result = 31 * result + (col != null ? col.hashCode() : 0);
         result = 31 * result + (user != null ? user.hashCode() : 0);
         return result;
-    }
-
-    @ManyToOne
-    @JoinColumn(name = "dept", referencedColumnName = "id")
-    public BaseInfo getDept() {
-        return dept;
-    }
-
-    public void setDept(BaseInfo dept) {
-        this.dept = dept;
     }
 
     @ManyToOne
